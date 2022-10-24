34db0228c9887deec2565b9df91ee7953e8dd9b7
Tibor22 <tothtibitravel@gmail.com>
2022-10-24 22:19:02 +0100

    setting up k8s nodeport and clusterIP -2

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,31 +1,31 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 
 const events = [];
 
-app.post('/events', async (req, res) => {
+app.post("/events", async (req, res) => {
 	const event = req.body;
 	events.push(event);
 	try {
-		await axios.post('http://localhost:4000/events', event); //posts
-		await axios.post('http://localhost:4001/events', event); //comments
-		await axios.post('http://localhost:4002/events', event); //query
-		await axios.post('http://localhost:4003/events', event); //moderation
+		await axios.post("http://posts-clusterip-srv:4000", event); //posts
+		await axios.post("http://localhost:4001/events", event); //comments
+		await axios.post("http://localhost:4002/events", event); //query
+		await axios.post("http://localhost:4003/events", event); //moderation
 
-		res.send({ status: 'OK' });
+		res.send({ status: "OK" });
 	} catch (e) {
 		console.log(e);
 	}
 });
 
-app.get('/events', async (req, res) => {
+app.get("/events", async (req, res) => {
 	res.send(events);
 });
 
 app.listen(4005, () => {
-	console.log('listening on port:4005');
+	console.log("listening on port:4005");
 });
--- posts/index.js
+++ posts/index.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const { randomBytes } = require('crypto');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const axios = require('axios');
+const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
+app.get("/posts", (req, res) => {
 	res.send(posts);
 });
-app.post('/posts', async (req, res) => {
+app.post("/posts", async (req, res) => {
 	try {
-		const id = randomBytes(4).toString('hex');
+		const id = randomBytes(4).toString("hex");
 		const { title } = req.body;
 		posts[id] = { title: title, id };
 
-		await axios.post('http://localhost:4005/events', {
-			type: 'PostCreated',
+		await axios.post("http://event-bus-srv:4005", {
+			type: "PostCreated",
 			data: { id, title },
 		});
 
@@ -29,12 +29,13 @@ app.post('/posts', async (req, res) => {
 	}
 });
 
-app.post('/events', (req, res) => {
-	console.log('Received Event:', req.body.type);
+app.post("/events", (req, res) => {
+	console.log("Received Event:", req.body.type);
 
 	res.send({});
 });
 
 app.listen(4000, () => {
-	console.log('listening on port:4000');
+	console.log("v55");
+	console.log("listening on port:4000");
 });
